53aca7b06734e3fe647ea94633bfed78d35db3ac
ngutruong <ngutruong@users.noreply.github.com>
2019-02-07 02:52:56 -0500

    Web API with MVC and Entity Framework
    Using CRUD for the Web API and the entity data model from an existing database

---
+++ Web API/CrudWebApi/CrudMvc/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using CrudMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            IEnumerable<MVCProductModel> prodList;
+            HttpResponseMessage response = ProductWebClient.WebApiClient.GetAsync("Product").Result;
+            prodList = response.Content.ReadAsAsync<IEnumerable<MVCProductModel>>().Result;
+            return View();
+        }
+    }
+}
---
+++ Web API/CrudWebApi/CrudMvc/Models/MVCProductModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudMvc.Models
+{
+    public class MVCProductModel
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
